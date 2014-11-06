bf7355cd55d37ce0f536269c0f0ff1412551f309
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 15:19:08 +0100

    Migrate to version 3.4

--- tests/test_aeat_303_es.py
+++ tests/test_aeat_303_es.py
@@ -4,21 +4,18 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class Aeat303EsTestCase(unittest.TestCase):
-    '''
-    Test AEAT 303 ES module.
-    '''
+    'Test AEAT 303 ES module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('aeat_303_es')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -27,6 +24,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             Aeat303EsTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_aeat303.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
